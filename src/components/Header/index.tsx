137ba33a0f5f9ff3ef44fffa07bd05866010e251
Italo Aurelio <italoaurelior@gmail.com>
2021-05-01 15:14:16 -0300

    Add link to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <img src="/logo.svg" alt="logo" />
-    </header>
+    <Link href="/">
+      <a>
+        <header className={styles.header}>
+          <img src="/logo.svg" alt="logo" />
+        </header>
+      </a>
+    </Link>
   );
 }
