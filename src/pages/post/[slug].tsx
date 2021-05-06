9e39baa8e979fb2685f84c9bbdeaa37d67a2eb41
Italo Aurelio <italoaurelior@gmail.com>
2021-05-05 21:32:26 -0300

    Update code

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -118,7 +118,6 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
-
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(slug), {});
 
